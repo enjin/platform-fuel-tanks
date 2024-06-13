0f9589e32dba373194693e416071632536a62fbc
Leonardo Custodio <leonardo@custodio.me>
2024-06-13 14:00:46 -0300

    Fixes

--- src/Services/Processor/Substrate/Events/Implementations/FuelTanks/FreezeStateMutated.php
+++ src/Services/Processor/Substrate/Events/Implementations/FuelTanks/FreezeStateMutated.php
@@ -25,7 +25,7 @@ class FreezeStateMutated extends FuelTankSubstrateEvent
         // Fail if it doesn't find the fuel tank
         $fuelTank = $this->getFuelTank($this->event->tankId);
 
-        if (empty($this->event->ruleSetId)) {
+        if (is_null($this->event->ruleSetId)) {
             $fuelTank->is_frozen = $this->event->isFrozen;
             $fuelTank->save();
 
@@ -40,7 +40,7 @@ class FreezeStateMutated extends FuelTankSubstrateEvent
 
     public function log(): void
     {
-        if (empty($this->event->ruleSetId)) {
+        if (is_null($this->event->ruleSetId)) {
             Log::debug(
                 sprintf(
                     'FuelTank %s was %s.',
