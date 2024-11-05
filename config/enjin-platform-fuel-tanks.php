d83bbe9133d4e8b73f88c3ab4f29473a811df518
Abner Tudtud <abner@enjin.io>
2024-11-05 13:13:38 +0800

    Add custom queue

--- config/enjin-platform-fuel-tanks.php
+++ config/enjin-platform-fuel-tanks.php
@@ -1,4 +1,13 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Queue
+    |--------------------------------------------------------------------------
+    |
+    | Here you may set the dedicated queue for this package
+    |
+    */
+    'queue' => env('PLATFORM_FUEL_TANKS_QUEUE', 'default'),
 ];
